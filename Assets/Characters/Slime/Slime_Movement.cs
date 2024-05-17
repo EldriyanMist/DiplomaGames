2f60a4cd47c0dea15f5f6e1b2adf28fbfedf36bc
Suienish <aldaniw9854@gmail.com>
2024-05-17 15:01:18 +0500

    slime dead and dropping meat added

--- Assets/Characters/Slime/Slime_Movement.cs
+++ Assets/Characters/Slime/Slime_Movement.cs
@@ -31,7 +31,7 @@ public class SlimeMovement : MonoBehaviour
         else
         {
             rb.velocity = Vector2.zero;
-            animator.Play("Slime_idle");
+            animator.SetBool("is_moving", false);
         }
 
         // Check for 'D' key press to trigger die animation
@@ -39,11 +39,18 @@ public class SlimeMovement : MonoBehaviour
         {
             Die();
         }
+
+        // Check for 'H' key press to trigger hit animation
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TriggerHit();
+        }
     }
 
     void MoveSlime()
     {
         rb.velocity = moveDirection * moveSpeed;
+        animator.SetBool("is_moving", true);
     }
 
     IEnumerator ChangeState()
@@ -59,6 +66,8 @@ public class SlimeMovement : MonoBehaviour
             }
             else
             {
+                animator.SetBool("is_moving", false);
+                animator.SetBool("isJumpingLeft", false); // Ensure isJumpingLeft is false when idle
                 yield return new WaitForSeconds(2f); // Idle for 2 seconds
             }
         }
@@ -72,19 +81,23 @@ public class SlimeMovement : MonoBehaviour
         {
             case 0:
                 moveDirection = Vector2.up; // Move up
-                animator.Play("slime_jump");
+                animator.SetBool("isJumpingLeft", false);
+                animator.SetBool("is_moving", true);
                 break;
             case 1:
                 moveDirection = Vector2.down; // Move down
-                animator.Play("slime_jump");
+                animator.SetBool("isJumpingLeft", false);
+                animator.SetBool("is_moving", true);
                 break;
             case 2:
                 moveDirection = Vector2.left; // Move left
-                animator.Play("slime_jump_left");
+                animator.SetBool("isJumpingLeft", true);
+                animator.SetBool("is_moving", true);
                 break;
             case 3:
                 moveDirection = Vector2.right; // Move right
-                animator.Play("slime_jump");
+                animator.SetBool("isJumpingLeft", false);
+                animator.SetBool("is_moving", true);
                 break;
         }
     }
@@ -94,17 +107,33 @@ public class SlimeMovement : MonoBehaviour
         // Handle collision with other objects
         if (collision.gameObject.tag == "Obstacle")
         {
-            animator.Play("slime_damage");
+            animator.SetBool("Hit", true);
             // Implement damage logic
         }
     }
 
+    public void TriggerHit()
+{
+    Debug.Log("TriggerHit called");
+    animator.SetBool("Hit", true);
+    animator.SetBool("isJumpingLeft", false);
+    animator.SetBool("is_moving", false);
+    
+
+    StartCoroutine(ResetHitState());
+}
+
+IEnumerator ResetHitState()
+{
+    yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+    animator.SetBool("Hit", false);
+}
+
+
     public void Die()
     {
-        animator.Play("slime_dead");
-        //logging message 
-        Debug.Log("Slime has died");
-        // Start a coroutine to wait until the end of the animation
+        animator.SetBool("Hit", true);
+        animator.SetBool("Defeated", true);
         StartCoroutine(DieCoroutine());
     }
 
